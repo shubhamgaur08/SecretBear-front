1867346f14e1a7f1af7c08050f96bf2bc331db50
shubham gaur <shubhamgaur672@gmail.com>
2024-01-17 18:38:01 +0530

    7

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -34,19 +34,19 @@ const Signup = (props) => {
     }
     return (
 //             
-<div className="container h-100 mt-3">
+<div className="container h-100 mt-5">
     <div className="row d-flex justify-content-center align-items-center h-100">
       <div className="col-lg-12 col-xl-11">
         <div className="card text-black" >
-          <div className="card-body p-md-5">
+          <div className="card-body ">
             <div className="row justify-content-center">
               <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
-                <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
+                <p className="text-center h1 fw-bold mb-2 mx-1 mx-md-4 ">Sign up</p>
 
                 <form className="mx-1 mx-md-4" onSubmit={handleSubmit}>
 
-                  <div className="d-flex flex-row align-items-center mb-4">
+                  <div className="d-flex flex-row align-items-center mb-2">
                     <i className="fas fa-user fa-lg me-3 fa-fw"></i>
                     <div className="form-outline flex-fill mb-0">
                       <input type="text" id="form3Example1c"  value={credentials.name} onChange={onChange} name="name" className="form-control" required minLength={5}/>
@@ -54,7 +54,7 @@ const Signup = (props) => {
                     </div>
                   </div>
 
-                  <div className="d-flex flex-row align-items-center mb-4">
+                  <div className="d-flex flex-row align-items-center mb-2">
                     <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
                     <div className="form-outline flex-fill mb-0">
                       <input type="email" id="form3Example3c"  value={credentials.email} onChange={onChange} name="email" className="form-control" />
@@ -62,7 +62,7 @@ const Signup = (props) => {
                     </div>
                   </div>
 
-                  <div className="d-flex flex-row align-items-center mb-4">
+                  <div className="d-flex flex-row align-items-center mb-2">
                     <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
                     <div className="form-outline flex-fill mb-0">
                       <input type="password" id="form3Example4c"  value={credentials.password} name="password" onChange={onChange} className="form-control" required minLength={5} />
@@ -70,7 +70,7 @@ const Signup = (props) => {
                     </div>
                   </div>
 
-                  <div className="d-flex flex-row align-items-center mb-4">
+                  <div className="d-flex flex-row align-items-center mb-2">
                     <i className="fas fa-key fa-lg me-3 fa-fw"></i>
                     <div className="form-outline flex-fill mb-0">
                           <input type="cpassword" value={credentials.cpassword} onChange={onChange} className="form-control" id="cpassword" name="cpassword" required minLength={5}/>
